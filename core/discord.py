448994ea4db69a09bd3c5ce1d98ad55641100351
invalidcode <78639481+invalidcode232@users.noreply.github.com>
2021-10-13 08:54:16 +0700

    Actually working ConnectionError fix

--- core/discord.py
+++ core/discord.py
@@ -1,6 +1,8 @@
 import requests
 import time
 
+import urllib3.exceptions
+
 
 class Discord:
     def __init__(self, token):
@@ -13,18 +15,25 @@ class Discord:
         }
 
         # Check if token is valid
-        user_data = requests.get(f"https://discord.com/api/users/@me", headers=self.header)
-        if user_data.status_code != 200:
-            raise Exception(f"Error while logging in with token | Status code: {user_data.status_code}")
+        try:
+            user_data = requests.get(f"https://discord.com/api/users/@me", headers=self.header)
+        except requests.exceptions.ConnectionError:
+            print("Connection error, this could be an issue with the server or your connection..")
         else:
-            user_json = user_data.json()
-            print(f"Logged in successfully as {user_json['username']}#{user_json['discriminator']} ({user_json['id']})")
+            if user_data.status_code != 200:
+                raise Exception(f"Error while logging in with token | Status code: {user_data.status_code}")
+            else:
+                user_json = user_data.json()
+                print(f"Logged in successfully as {user_json['username']}#{user_json['discriminator']} ({user_json['id']})")
 
     def typing(self, channel):
-        response = requests.post(f"https://discord.com/api/v9/channels/{channel}/typing", headers=self.header)
-
-        if response.status_code >= 300:
-            raise Exception(f"An unknown error occurred while typing | Status code: {response.status_code}")
+        try:
+            response = requests.post(f"https://discord.com/api/v9/channels/{channel}/typing", headers=self.header)
+        except requests.exceptions.ConnectionError:
+            print("Connection error, this could be an issue with the server or your connection..")
+        else:
+            if response.status_code >= 300:
+                raise Exception(f"An unknown error occurred while typing | Status code: {response.status_code}")
 
     def send_message(self, channel, content):
         self.typing(channel)
@@ -35,7 +44,11 @@ class Discord:
             "content": content,
             "tts": "false"
         }
-        response = requests.post(f"https://discord.com/api/v9/channels/{channel}/messages", json=payload, headers=self.header)
 
-        if response.status_code != 200:
-            raise Exception(f"An unknown error occurred while sending message | Status code: {response.status_code}")
+        try:
+            response = requests.post(f"https://discord.com/api/v9/channels/{channel}/messages", json=payload, headers=self.header)
+        except requests.exceptions.ConnectionError:
+            print("Connection error, this could be an issue with the server or your connection..")
+        else:
+            if response.status_code != 200:
+                raise Exception(f"An unknown error occurred while sending message | Status code: {response.status_code}")
--- index.py
+++ index.py
@@ -13,41 +13,37 @@ with open("config/config.json", "r") as json_data:
     client = Discord(config['token'])
     autominer = Autominer(client, config['channel_id'])
 
-    try:
-        # Auto-sell
-        # You must enable auto-sell in order for auto-upgrade and auto-rebirth to work
-        threading.Thread(target=autominer.auto_sell, kwargs={
-            'randomization': config['auto_sell']['randomization'],
-            'base': config['auto_sell']['base'],
-            'auto_upgrade': config['auto_upgrade']['enabled'],
-            'auto_rebirth': config['auto_rebirth']['enabled'],
-            'upgrade_after': config['auto_upgrade']['upgrade_after'],
-            'rebirth_after': config['auto_rebirth']['rebirth_after']
-        }).start()
+    # Auto-sell
+    # You must enable auto-sell in order for auto-upgrade and auto-rebirth to work
+    threading.Thread(target=autominer.auto_sell, kwargs={
+        'randomization': config['auto_sell']['randomization'],
+        'base': config['auto_sell']['base'],
+        'auto_upgrade': config['auto_upgrade']['enabled'],
+        'auto_rebirth': config['auto_rebirth']['enabled'],
+        'upgrade_after': config['auto_upgrade']['upgrade_after'],
+        'rebirth_after': config['auto_rebirth']['rebirth_after']
+    }).start()
 
-        # Auto-fish
-        threading.Thread(target=autominer.auto_fish, kwargs={
-            'randomization': config['auto_fish']['randomization'],
-            'base': config['auto_fish']['base'],
-        }).start()
+    # Auto-fish
+    threading.Thread(target=autominer.auto_fish, kwargs={
+        'randomization': config['auto_fish']['randomization'],
+        'base': config['auto_fish']['base'],
+    }).start()
 
-        # Auto-hunt
-        threading.Thread(target=autominer.auto_hunt, kwargs={
-            'randomization': config['auto_hunt']['randomization'],
-            'base': config['auto_hunt']['base'],
-        }).start()
+    # Auto-hunt
+    threading.Thread(target=autominer.auto_hunt, kwargs={
+        'randomization': config['auto_hunt']['randomization'],
+        'base': config['auto_hunt']['base'],
+    }).start()
 
-        # Auto-quiz
-        threading.Thread(target=autominer.auto_quiz, kwargs={
-            'randomization': config['auto_quiz']['randomization'],
-            'base': config['auto_quiz']['base'],
-        }).start()
+    # Auto-quiz
+    threading.Thread(target=autominer.auto_quiz, kwargs={
+        'randomization': config['auto_quiz']['randomization'],
+        'base': config['auto_quiz']['base'],
+    }).start()
 
-        # Auto-hourly
-        threading.Thread(target=autominer.auto_hourly, kwargs={
-            'randomization': config['auto_hourly']['randomization'],
-            'base': config['auto_hourly']['base'],
-        }).start()
-    except ConnectionError:
-        print("Connection error, reloading thread...")
-        print("If this happens constantly, please check your internet connection and try again")
+    # Auto-hourly
+    threading.Thread(target=autominer.auto_hourly, kwargs={
+        'randomization': config['auto_hourly']['randomization'],
+        'base': config['auto_hourly']['base'],
+    }).start()
